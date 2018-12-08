ac3d54370e01d3fbe7cbd3bdfece3433a226df56
nitaivi <38476668+nitaivi@users.noreply.github.com>
2018-12-08 19:10:50 +0200

    Add files via upload

---
+++ PreProcessing.py
@@ -0,0 +1,34 @@
+import os
+from shutil import copyfile
+import scipy.io as sio
+
+path = './Mat_Files '
+patients = []
+number_of_images = []
+
+new_dir = './Filtered_Mat_Files'
+if not os.path.exists(new_dir):
+    os.makedirs(new_dir)
+
+
+for root, dirs, files in os.walk(path):
+    for filename in files
+        pulse = filename.split('pulse_')
+        if pulse[1].startswith('_'):
+            continue
+        elif float(pulse[1])>150 or float(pulse[1])<50:
+                continue
+        sys = filename.split('sys_')
+        if sys[1].startswith('_'):
+            continue
+        elif float(sys[1])>200 or float(sys[1])<50:
+                continue
+        dias = filename.split('dias_')
+        if dias[1].startswith('_'):
+            continue
+        elif float(dias[1])>150 or float(dias[1])<30:
+                continue
+        else:
+            src = root + '/' + filename
+            dst = new_dir + '/' + filename
+            copyfile(src, dst)
